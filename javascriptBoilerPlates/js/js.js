fdc32705e9828f4ca40bd5057483dc783d95c434
BilyanaK <eaabpk@students.eaaa.dk>
2020-02-05 08:47:59 +0100

    in class wednesday
    trying to push

--- javascriptBoilerPlates/js/js.js
+++ javascriptBoilerPlates/js/js.js
@@ -3,3 +3,26 @@
  * purpose: interactivity
  **/
 console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
+
+//paste your javascript here
+
+var myGallery = [
+  "watermelon.jpg",
+  "fish.jpg",
+  "kite.jpg",
+  "blue summer.jpg",
+  "feet.jpg",
+  "wild beach.jpg",
+  "samsoisland.jpg",
+  "poppies.jpg"
+
+];
+
+//create the html
+
+for (i = 0; i < myGallery.length; i++) {
+    // note the shorthand code below
+    exhibition.innerHTML += '<img src="images/' +
+        myGallery[i] +
+        '" alt="from my gallery">';
+}
